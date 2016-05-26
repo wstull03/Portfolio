18ab839d33a520dc2cb50254e32a0cc59cdba450
Your Name <you@example.com>
2016-05-26 11:51:24 -0400

    setting up repo

---
+++ Portfolio/Models/BuddyClasses/ProjectsBuddy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Portfolio.Models
+{
+    [MetadataType(typeof(ProjectsBuddy))]
+    public partial class Project
+    {
+        public string ProjectListing
+        {
+            get { return this.ProjectName + ":"; }
+            set { }
+        }
+    }
+
+    public class ProjectsBuddy
+    {
+
+        public int ProjectID { get; set; }
+
+        [Display(Name = "Project Name")]
+        public string ProjectName { get; set; }
+
+        [Display(Name = "GitHub Link")]
+        public string GitHubLink { get; set; }
+
+    }
+}
